e6479b36aefc22a2c4edbb5dd3a35e2aafefd556
Callmesammy <callmesammy@yahoo.com>
2023-12-30 19:31:49 +0100

    s

--- Src/HomePage.js
+++ Src/HomePage.js
@@ -1,166 +1,74 @@
 import {
   View,
-  Text,
   Image,
-  StatusBar,
+  Text,
   SafeAreaView,
-  TextInput,
-  TouchableOpacity,
+  StatusBar,
   ScrollView,
+  TouchableOpacity,
+  FlatList,
 } from "react-native";
 import React, { useState } from "react";
-import Icon from "react-native-vector-icons/Ionicons";
-
+import Icon from "react-native-vector-icons/Feather";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+import { Fontss, Imag } from "../Theme/Fonts";
+import Feattures from "./Feattures";
+import Scroller from "./Scroller";
 
 export default function HomePage() {
-  const [hide, Hfeat] = useState(false);
-  const [location, locate] = useState([1, 2, 3]);
-
-  const preasable = (loc) => {
-    console.log("location: ", loc);
-  };
+  const [flex, flexbox] = useState(null);
   return (
-    <View className="flex-1 relative">
+    <View className="flex-1 relative bg-black">
       <StatusBar barStyle="light-content" />
-      <Image
-        blurRadius={35}
-        source={require("../assets/images/bg.png")}
-        className="absolute w-full h-full "
-      />
-      <SafeAreaView className="flex flex-1">
-        <View style={{ height: "7%" }} className="mx-4 relative z-50">
-          <View
-            style={{
-              backgroundColor: hide ? "white" : "transparent",
-              opacity: "0.5",
-            }}
-            className="flex-row rounded-full items-center  justify-end "
-          >
-            {hide ? (
-              <TextInput
-                placeholder="Search here"
-                placeholderTextColor="gray"
-                className="pl-6 h-10 flex-1 pb-1 text-base text-black items-center"
-              />
-            ) : null}
-            <TouchableOpacity
-              onPress={() => Hfeat(!hide)}
-              className="ml-4 rounded-full p-3 bg-neutral-300 m-1 items-center justify-center opacity-70"
-            >
-              <Icon name="search" size="25" color="gray" className="" />
+
+      <SafeAreaView>
+        <View className="flex-row justify-between space-x-3  px-2">
+          <Text className="text-white text-3xl font-bold">Good Evening </Text>
+          <View className="flex-row space-x-4">
+            <TouchableOpacity>
+              <Icon name="bell" size="25" color="white" />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Icon name="settings" size="25" color="white"/>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Icon name="clock" size="25" color="white" />
             </TouchableOpacity>
           </View>
-          {location.length > 0 && hide ? (
-            <View className="absolute top-14 bg-gray-300 w-full rounded-3xl">
-              {location.map((loc, rock) => {
-                var Showcase = rock + 1 != location.length;
-                var showBase = Showcase
-                  ? "border-0 border-b-2 border-b-gray-400 "
-                  : "";
-                return (
-                  <TouchableOpacity
-                    onPress={() => preasable(loc)}
-                    key={rock}
-                    className={
-                      "px-4 p-4 items-center space-x-1 flex-row " + showBase
-                    }
+        </View>
+
+        <View className="top-7 space-y-5">
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            className="overflow-visible top-3"
+          >
+            {Fontss.map((cat) => {
+              let OnKey = cat == flex;
+              let pressed = OnKey ? " bg-green-500" : " bg-gray-600";
+              let texted = OnKey ? " text-black" : " text-white";
+
+              return (
+                <TouchableOpacity
+                  key={cat}
+                  onPress={() => flexbox(cat)}
+                  className={"rounded-full ml-3 bg-gray-600 p-3" + pressed}
+                >
+                  <Text
+                    style={{ fontSize: wp(4) }}
+                    className={"font-bold tracking-wider text-white" + texted}
                   >
-                    <Icon name="pin" size="18" color="gray" />
-                    <Text className="text-center text-lg ">
-                      Australia, UpHill
-                    </Text>
-                  </TouchableOpacity>
-                );
-              })}
-            </View>
-          ) : null}
+                    {cat}
+                  </Text>
+                </TouchableOpacity>
+              );
+            })}
+          </ScrollView>
+          <Feattures />
         </View>
-        <View className="flex-1 flex mb-2 justify-around mx-4">
-          <Text className="text-2xl font-bold text-gray-300 text-center">
-            Australia
-                      <Text className="text-gray-400 text-xl"> UpHil </Text>
-
-          </Text>
-          <View className="flex-row justify-center">
-            <Image source={require('../assets/images/partlycloudy.png')} className="w-52 h-52"/>
-          </View>
-        
-        <View className="space-y-2">
-          <Text className="text-6xl text-white font-bold text-center">23°</Text>
-          <Text className=" tracking-widest text-2xl text-center text-gray-400 ">Partly Cloudy</Text>
-        </View>
-
-        <View className="flex-row justify-between mx-2">
-          <View className="flex-row space-x-2 items-center">
-            <Image source={require('../assets/icon/wind.png')} className="w-6 h-6"/>
-            <Text className="text-gray-300 font-semibold text-base">
-              28km
-            </Text>
-            </View>
-            <View className="flex-row space-x-2 items-center">
-            <Image source={require('../assets/icon/drop.png')} className="w-6 h-6"/>
-            <Text className="text-gray-300 font-semibold text-base">
-              30%
-            </Text>
-            </View>
-            <View className="flex-row space-x-2 items-center">
-            <Image source={require('../assets/icon/sun.png')} className="w-6 h-6"/>
-            <Text className="text-gray-300 font-semibold text-base">
-              9:30PM
-            </Text>
-            </View>
-          </View>
-        </View>
-        <View className=" justify-center ">
-        <View className="flex-row ml-5 space-x-2  items-center">
-        <Icon name="calendar-outline" size="25" color="white"/>
-        <Text className="text-gray-300 font-semibold text-md"> Daily Forecast</Text>
-      
-        </View>
-        <ScrollView
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={{padding: 20}}
-        className="space-x-3"
-        >
-        <View className="p-4 pl-2 py-2 flex bg-gray-500 space-x-3 justify-center items-center rounded-3xl">
-        <Image source={require('../assets/images/heavyrain.png')} className="w-11 h-11"/>
-        <Text className="text-gray-300 font-semibold text-center">Monday</Text>
-        <Text  className="text-white font-semibold text-center">23°</Text>
-        </View>
-        <View className="p-4 pl-2 py-2 flex bg-gray-500 space-x-3 justify-center items-center rounded-3xl">
-        <Image source={require('../assets/images/heavyrain.png')} className="w-11 h-11"/>
-        <Text className="text-gray-300 font-semibold text-center">Monday</Text>
-        <Text  className="text-white font-semibold text-center">23°</Text>
-        </View>
-        <View className="p-4 pl-2 py-2 flex bg-gray-500 space-x-3 justify-center items-center rounded-3xl">
-        <Image source={require('../assets/images/heavyrain.png')} className="w-11 h-11"/>
-        <Text className="text-gray-300 font-semibold text-center">Monday</Text>
-        <Text  className="text-white font-semibold text-center">23°</Text>
-        </View>
-        <View className="p-4 pl-2 py-2 flex bg-gray-500 space-x-3 justify-center items-center rounded-3xl">
-        <Image source={require('../assets/images/heavyrain.png')} className="w-11 h-11"/>
-        <Text className="text-gray-300 font-semibold text-center">Monday</Text>
-        <Text  className="text-white font-semibold text-center">23°</Text>
-        </View>
-        <View className="p-4 pl-2 py-2 flex bg-gray-500 space-x-3 justify-center items-center rounded-3xl">
-        <Image source={require('../assets/images/heavyrain.png')} className="w-11 h-11"/>
-        <Text className="text-gray-300 font-semibold text-center">Monday</Text>
-        <Text  className="text-white font-semibold text-center">23°</Text>
-        </View>
-        <View className="p-4 pl-2 py-2 flex bg-gray-500 space-x-3 justify-center items-center rounded-3xl">
-        <Image source={require('../assets/images/heavyrain.png')} className="w-11 h-11"/>
-        <Text className="text-gray-300 font-semibold text-center">Monday</Text>
-        <Text  className="text-white font-semibold text-center">23°</Text>
-        </View>
-        <View className="p-4 pl-2 py-2 flex bg-gray-500 space-x-3 justify-center items-center rounded-3xl">
-        <Image source={require('../assets/images/heavyrain.png')} className="w-11 h-11"/>
-        <Text className="text-gray-300 font-semibold text-center">Monday</Text>
-        <Text  className="text-white font-semibold text-center">23°</Text>
-        </View>
-        </ScrollView>
-        </View> 
-       
       </SafeAreaView>
     </View>
   );
---
+++ Theme/Fonts.js
@@ -0,0 +1,50 @@
+export const Fontss =[
+
+        'All', 'Family',  'Adventure', 'Racing', 'Education', 
+
+]
+
+ 
+export const Items = [
+        {
+          id: 1,
+          name: "Flavour",
+          image: require("../assets/Film/1.jpg"),
+        },
+        {
+          id: 2,
+          name: "Top 100 Nigeria",
+          image: require("../assets/Film/2.jpg"),
+        },
+        {
+          id: 3,
+          name: "Asake",
+          image: require("../assets/Film/3.jpg"),
+        },
+        {
+          id: 4,
+          name: "Top Music",
+          image: require("../assets/Film/4.jpg"),
+        },
+        {
+          id: 5,
+          name: "Sounds",
+          image: require("../assets/Film/5.jpg"),
+        },
+        {
+          id: 6,
+          name: "Street Music",
+          image: require("../assets/Film/6.jpg"),
+        },
+        {
+          id: 4,
+          name: "Just Sounds",
+          image: require("../assets/Film/4.jpg"),
+        },
+        {
+          id: 5,
+          name: "Top 10",
+          image: require("../assets/Film/5.jpg"),
+        },
+      
+      ];
